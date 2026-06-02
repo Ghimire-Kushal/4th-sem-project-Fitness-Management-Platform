0f5505a3e1e941e4209028d9adc164d2725da753
Kushal Ghimire <kushal.upr@gmail.com>
2026-06-02 22:55:33 +0545

    prepare for deployment: env-based API URL, CORS config, Vercel routing

--- server/server.js
+++ server/server.js
@@ -6,7 +6,10 @@ const connectDB = require("./config/db");
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || "*",
+  credentials: true,
+}));
 app.use(express.json());
 
 // Routes
